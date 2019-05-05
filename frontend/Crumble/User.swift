67667931c5506276a2c3e9e34ef14f65c6282386
Nathan <nts28@cornell.edu>
2019-05-05 17:56:14 -0400

    pushing

---
+++ frontend/Crumble/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Crumble
+//
+//  Created by Nathan Stack on 5/5/19.
+//  Copyright © 2019 Beth Mieczkowski. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    //var 
+    
+}
